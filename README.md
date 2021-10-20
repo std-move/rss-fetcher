25aabbc0a0ebcce4d1e36b4fea004757833f90a7
Anonymous <26625259+std-move@users.noreply.github.com>
2021-10-20 15:33:13 +0200

    minor code improvements

---
+++ README.md
@@ -0,0 +1,5 @@
+# RSS fetcher
+
+This repository currently provides a feed of "bigTop" WSJ articles from section World. These Big Top articles are usually well researched and interesting, however there is no official feed for them. The feed is created by fetching WSJ's World News RSS feed and filtering it, leaving in only these in-depth articles. Only data that is in the original feed gets into the filtered feed - no scraping/article text extraction is attempted.
+
+The feed is present in branch gh-pages and is updated every 2 hours.
--- main.go
+++ main.go
@@ -13,18 +13,16 @@ import (
 )
 
 func main() {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://feeds.a.dj.com/rss/RSSWorldNews.xml")
+	origFeed, err := gofeed.NewParser().ParseURL("https://feeds.a.dj.com/rss/RSSWorldNews.xml")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("failed to fetch feed: ", err)
 	}
-	// log.Println(feed.Title)
 
 	now := time.Now()
 	myFeed := &feeds.Feed{
-		Title:       feed.Title + " - bigTop stories",
+		Title:       origFeed.Title + " - bigTop stories",
 		Link:        &feeds.Link{Href: "https://std-move.github.io/rss-fetcher/wsj-world-bigtop.xml"},
-		Description: feed.Description,
+		Description: origFeed.Description,
 		Created:     now,
 	}
 
@@ -32,31 +30,34 @@ func main() {
 		Timeout: 10 * time.Second,
 	}
 
-	for _, itm := range feed.Items {
-		origLink := itm.Link
+	for _, itm := range origFeed.Items {
+		var ampLink string
 		if !strings.Contains(itm.Link, "wsj.com/amp/") {
-			itm.Link = strings.Replace(itm.Link, "wsj.com/", "wsj.com/amp/", -1)
+			ampLink = strings.Replace(itm.Link, "wsj.com/", "wsj.com/amp/", -1)
+		} else {
+			ampLink = itm.Link
 		}
-		req, err := http.NewRequest("GET", itm.Link, nil)
+		req, err := http.NewRequest("GET", ampLink, nil)
 		if err != nil {
-			log.Println("error creating: ", itm.Link, err)
+			log.Println("error creating req: ", ampLink, err)
 			continue
 		}
 		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0")
 		rsp, err := client.Do(req)
 		if err != nil {
-			log.Println("error fetching: ", itm.Link, err)
+			log.Println("error fetching: ", ampLink, err)
 			continue
 		}
 		body, err := io.ReadAll(rsp.Body)
 		if err != nil {
-			log.Println("error reading: ", itm.Link, err)
+			log.Println("error reading: ", ampLink, err)
 			continue
 		}
 		if !bytes.Contains(body, []byte("class=\"bigTop-hero")) {
+			log.Println("not a bigTop article: ", ampLink)
 			continue
 		}
-		log.Println("bigTop link: ", itm.Link)
+		log.Println("bigTop article: ", itm.Link)
 
 		var created time.Time
 		if itm.PublishedParsed != nil {
@@ -72,11 +73,11 @@ func main() {
 			updated = created
 		}
 		myFeed.Items = append(myFeed.Items, &feeds.Item{
-			Title:       itm.Title,
-			Link:        &feeds.Link{Href: origLink},
-			Content:     itm.Description,
-			Created:     created,
-			Updated:	 updated,
+			Title:   itm.Title,
+			Link:    &feeds.Link{Href: itm.Link},
+			Content: itm.Description,
+			Created: created,
+			Updated: updated,
 		})
 	}
 
@@ -85,10 +86,13 @@ func main() {
 		log.Fatal("failed to serialize my feed: ", err)
 	}
 
-	_ = os.MkdirAll("public", os.ModePerm)
+	err = os.MkdirAll("public", os.ModePerm)
+	if err != nil {
+		log.Fatal("failed to mkdir: ", err)
+	}
 	f, err := os.Create("public/wsj-world-bigtop.xml")
 	if err != nil {
-		log.Fatal("failed to serialize my feed: ", err)
+		log.Fatal("failed to create file: ", err)
 	}
 	defer f.Close()
 	_, err = f.WriteString(ser)
@@ -96,5 +100,5 @@ func main() {
 		log.Fatal("failed to write my feed: ", err)
 	}
 
-	log.Println(ser)
+	log.Println("Successfully updated feed, article count [", len(myFeed.Items), "] out of [", len(origFeed.Items), "]")
 }
